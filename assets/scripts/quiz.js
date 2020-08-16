dedf924b619ae7509909f0eb4efb5cc845aec103
imbingz <bingz@me.com>
2020-08-15 23:20:32 -0600

    change to 10 questions

--- assets/scripts/quiz.js
+++ assets/scripts/quiz.js
@@ -15,39 +15,39 @@ const quiz = [
 		option: [ 'call myFunction()', 'myFunction()', 'all function myFunction()', 'myFunction' ],
 		answer: 1
 	},
-	// {
-	// 	question: 'How to write an IF statement in JavaScript?',
-	// 	option: [ 'if i = 5 then', 'if i = 5', 'if i == 5 then', 'if (i == 5)' ],
-	// 	answer: 3
-	// },
-	// {
-	// 	question: 'How does a WHILE loop start?',
-	// 	option: [ 'while (i <= 10; i++)', 'while i = 1 to 10', 'while (i <= 10)', 'while(var i <= 10)' ],
-	// 	answer: 2
-	// },
-	// {
-	// 	question: 'How does a FOR loop start?',
-	// 	option: [ 'for (let i = 0; i <= 5; i++)', 'for (let i = 0; i <= 5)', 'for (i <= 5; i++)', 'for i = 1 to 5' ],
-	// 	answer: 0
-	// },
-	// {
-	// 	question: 'How do you round the number 7.25, to the nearest integer?',
-	// 	option: [ 'rnd(7.25)', 'Math.rnd(7.25)', 'Math.round(7.25)', 'round(7.25)' ],
-	// 	answer: 2
-	// },
-	// {
-	// 	question: 'How do you find the number with the highest value of x and y?',
-	// 	option: [ 'ceil(x, y)', 'Math.ceil(x, y)', 'Math.max(x, y)', 'top(x, y)' ],
-	// 	answer: 2
-	// },
-	// {
-	// 	question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
-	// 	option: [ 'if (i != 5)', 'if i <> 5', 'if (i <> 5)', 'wif i =! 5 then' ],
-	// 	answer: 0
-	// },
-	// {
-	// 	question: 'What will the following code return: Boolean(10 > 9)',
-	// 	option: [ 'false', 'true', 'NaN', '10 > 9' ],
-	// 	answer: 1
-	// }
+	{
+		question: 'How to write an IF statement in JavaScript?',
+		option: [ 'if i = 5 then', 'if i = 5', 'if i == 5 then', 'if (i == 5)' ],
+		answer: 3
+	},
+	{
+		question: 'How does a WHILE loop start?',
+		option: [ 'while (i <= 10; i++)', 'while i = 1 to 10', 'while (i <= 10)', 'while(var i <= 10)' ],
+		answer: 2
+	},
+	{
+		question: 'How does a FOR loop start?',
+		option: [ 'for (let i = 0; i <= 5; i++)', 'for (let i = 0; i <= 5)', 'for (i <= 5; i++)', 'for i = 1 to 5' ],
+		answer: 0
+	},
+	{
+		question: 'How do you round the number 7.25, to the nearest integer?',
+		option: [ 'rnd(7.25)', 'Math.rnd(7.25)', 'Math.round(7.25)', 'round(7.25)' ],
+		answer: 2
+	},
+	{
+		question: 'How do you find the number with the highest value of x and y?',
+		option: [ 'ceil(x, y)', 'Math.ceil(x, y)', 'Math.max(x, y)', 'top(x, y)' ],
+		answer: 2
+	},
+	{
+		question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+		option: [ 'if (i != 5)', 'if i <> 5', 'if (i <> 5)', 'wif i =! 5 then' ],
+		answer: 0
+	},
+	{
+		question: 'What will the following code return: Boolean(10 > 9)',
+		option: [ 'false', 'true', 'NaN', '10 > 9' ],
+		answer: 1
+	}
 ];
